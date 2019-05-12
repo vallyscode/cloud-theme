f5129842e479e78efc1389c1f8e81c6cd46c0473
Valerii Lysenko <vallyscode@gmail.com>
2019-05-12 21:47:12 +0000

    Changed colors, added grep, rainbow and other

--- cloud-theme.el
+++ cloud-theme.el
@@ -111,7 +111,7 @@
 ;; #7d57c2
 ;; #7c56c1
 ;; #855dcf
-;; #b48cff
+;; #b48cffy
 ;; #5e8203
 ;; #678f03
 ;; #7a9730
@@ -140,12 +140,12 @@
    `(default ((,class (:background "#f2f2f2" :foreground "#454545"))))
 
    ;; highlight
-   `(cursor ((,class (:background "#006c96" :foreground "#f2f2f2" ))))
+   `(cursor ((,class (:background "#00638a" :foreground "#f2f2f2" ))))
    `(fringe ((,class (:background "#f2f2f2" :foreground "#454545"  ))))
    `(border ((,class (:foreground "#2f7e9d"))))
    `(vertical-border ((,class (:foreground "#2f7e9d"))))
    `(highlight ((,class (:background "#fffbc4"))))
-   `(region ((,class (:background "#008abd" :foreground "#f2f2f2"))))
+   `(region ((,class (:background "#3389ab" :foreground "#f2f2f2"))))
    `(secondary-selection ((,class (:weight bold :background "#fffbc4"))))
    `(isearch ((,class (:foreground "#f2f2f2" :background "#008abd"))))
    `(isearch-fail ((,class (:weight bold :foreground "#f2f2f2" :background "#d0372d"))))
@@ -178,8 +178,8 @@
 
    ;; font lock
    `(font-lock-builtin-face ((,class (:foreground "#7d57c2"))))
-   `(font-lock-comment-delimiter-face ((,class (:weight normal :foreground "#8c8c8c"))))
-   `(font-lock-comment-face ((,class (:slant italic :weight normal :foreground "#8c8c8c"))))
+   `(font-lock-comment-delimiter-face ((,class (:weight normal :foreground "#919aa1"))))
+   `(font-lock-comment-face ((,class (:slant italic :weight normal :foreground "#919aa1"))))
    `(font-lock-constant-face ((,class (:foreground "#008080"))))
    `(font-lock-doc-face ((,class (:slant italic :weight normal :foreground "#678f03"))))
    `(font-lock-function-name-face ((,class (:foreground "#454545"))))
@@ -241,11 +241,23 @@
    `(diff-refine-changed ((,class (:background "#fce8c9"))))
    `(diff-header ((,class (:foreground "#8c8c8c" :background "#f2f2f2"))))
    `(diff-file-header ((,class (:foreground "#2f7e9d" :background "#f2f2f2"))))
+   `(diff-hunk-header ((,class (:foreground "#7d57c2" :background "#f2f2f2"))))
+   `(diff-index ((,class (:foreground "#8c8c8c" :background "#f2f2f2"))))
    `(diff-indicator-added ((,class (:foreground "#2e994c" :background "#ddffdd"))))
    `(diff-indicator-removed ((,class (:foreground "#d0372d" :background "#ffdddd"))))
    `(diff-indicator-changed ((,class (:foreground "#bf7000" :background "#f8f1d3"))))
-   `(diff-hunk-header ((,class (:foreground "#7d57c2" :background "#f2f2f2"))))
-   `(diff-none ((,class (:foreground "#454545"))))
+   `(diff-nonexistent ((,class (:foreground "#454545"))))
+
+   ;; ediff
+   `(ediff-current-diff-A ((,class (:background "#ffdddd"))))
+   `(ediff-current-diff-B ((,class (:background "#ddffdd"))))
+   `(ediff-current-diff-C ((,class (:background "#f8f1d3"))))
+   `(ediff-even-diff-A ((,class (:background "#cccccc"))))
+   `(ediff-even-diff-B ((,class (:background "#cccccc"))))
+   `(ediff-fine-diff-A ((,class (:background "#eecccc"))))
+   `(ediff-fine-diff-B ((,class (:background "#cceecc"))))
+   `(ediff-odd-diff-A ((,class (:background "#cccccc"))))
+   `(ediff-odd-diff-B ((,class (:background "#cccccc"))))
 
    ;; company
    `(company-tooltip-common-selection ((,class (:weight normal :foreground "#f2f2f2"))))
@@ -270,6 +282,29 @@
    ;; which-function
    `(which-func ((,class (:foreground "#7d57c2"))))
 
+   ;; undo-tree
+   `(undo-tree-visualizer-current-face ((,class (:foreground "#cc6d00"))))
+   `(undo-tree-visualizer-active-branch-face ((,class (:foreground "#b48cff"))))
+   `(undo-tree-visualizer-default-face ((,class (:foreground "#6c4ca8"))))
+   `(undo-tree-visualizer-register-face ((,class (:foreground "#678f03"))))
+   `(undo-tree-visualizer-unmodified-face ((,class (:foreground "#6c7378"))))
+
+   ;; grep
+   `(grep-context-face ((,class (:foreground "#919aa1"))))
+   `(grep-error-face ((,class (:foreground "#D0372d"))))
+   `(grep-hit-face ((,class (:foreground "#2f7e9d"))))
+   `(grep-match-face ((,class (:foreground nil :background nil :inherit match))))
+
+   `(regex-tool-matched-face ((,class (:foreground nil :background nil :inherit match))))
+
+   ;; compilation
+   `(compilation-column-number ((,class (:foreground "#cc6d00"))))
+   `(compilation-line-number ((,class (:foreground "#cc6d00"))))
+   `(compilation-message-face ((,class (:foreground "#2f7e9d"))))
+   `(compilation-mode-line-exit ((,class (:foreground "#2e994c"))))
+   `(compilation-mode-line-fail ((,class (:foreground "#D0372d"))))
+   `(compilation-mode-line-run ((,class (:foreground "#2f7e9d"))))
+
    ;; flycheck
    `(flycheck-info ((,class (:underline (:color "#2e994c" :style wave) :weight bold))))
    `(flycheck-warning ((,class (:underline (:color "#cc6d00" :style wave) :weight bold))))
@@ -292,23 +327,44 @@
    `(helm-selection-line ((,class (:background "#f2f2f2"))))
    `(helm-separator ((,class (:foreground "#454545"))))
    `(helm-visible-mark ((,class (:foreground "#D0372D" :background "#ffdddd"))))
-   `(helm-buffer-directory ((,class (:foreground "#008abd" :weight bold))))
+   `(helm-buffer-directory ((,class (:foreground "#2f7e9d" :weight normal))))
    `(helm-buffer-file ((,class (:foreground "#454545"))))
    `(helm-buffer-not-saved ((,class (:foreground "#f4ad49"))))
    `(helm-buffer-process ((,class (:foreground "#D0372D"))))
    `(helm-buffer-saved-out ((,class (:foreground "#D0372D"))))
-   `(helm-buffer-size ((,class (:foreground "#f4ad49"))))
-   `(helm-candidate-number ((,class (:foreground "#f2f2f2" :background "#008abd"))))
-   `(helm-ff-directory ((,class (:foreground "#008abd" :weight bold))))
+   `(helm-buffer-size ((,class (:foreground "#cc6d00"))))
+   `(helm-candidate-number ((,class (:foreground "#f2f2f2" :background "#cc6d00"))))
+   `(helm-ff-directory ((,class (:foreground "#2f7e9d" :weight bold))))
    `(helm-ff-executable ((,class (:foreground "#2e994c"))))
    `(helm-ff-file ((,class (:foreground "#454545"))))
    `(helm-ff-invalid-symlink ((,class (:foreground "#7d57c2" :background "#ffdddd"))))
    `(helm-ff-symlink ((,class (:foreground "#7d57c2"))))
 
-   ;; Outline
-   `(outline-1 ((,class (:height 1.0 :weight bold :foreground "#008abd"))))
-   `(outline-2 ((,class (:height 1.0 :weight bold :foreground "#7d57c2"))))
-   `(outline-3 ((,class (:height 1.0 :weight bold :foreground "#cc6d00"))))
+   ;; which-key
+   `(which-key-key-face ((,class (:foreground "#7d57c2" :weight bold))))
+   `(which-key-special-key-face ((,class (:foreground "#d70087"  :weight bold :height 1.1))))
+   `(which-key-command-description-face ((,class (:foreground "#454545" ))))
+   `(which-key-group-description-face ((,class (:foreground "#2f7e9d"))))
+   `(which-key-separator-face ((,class (:foreground "#919aa1"))))
+
+   ;; rainbow delimiters
+   `(rainbow-delimiters-depth-1-face ((,class (:foreground "#00638a"))))
+   `(rainbow-delimiters-depth-2-face ((,class (:foreground "#7d57c2"))))
+   `(rainbow-delimiters-depth-3-face ((,class (:foreground "#008080"))))
+   `(rainbow-delimiters-depth-4-face ((,class (:foreground "#2e994c"))))
+   `(rainbow-delimiters-depth-5-face ((,class (:foreground "#00638a"))))
+   `(rainbow-delimiters-depth-6-face ((,class (:foreground "#7d57c2"))))
+   `(rainbow-delimiters-depth-7-face ((,class (:foreground "#008080"))))
+   `(rainbow-delimiters-depth-8-face ((,class (:foreground "#2e994c"))))
+   `(rainbow-delimiters-depth-9-face ((,class (:foreground "#00638a"))))
+   `(rainbow-delimiters-depth-10-face ((,class (:foreground "#7d57c2"))))
+   `(rainbow-delimiters-depth-11-face ((,class (:foreground "#008080"))))
+   `(rainbow-delimiters-depth-12-face ((,class (:foreground "#2e994c"))))
+
+   ;; outline
+   `(outline-1 ((,class (:height 1.3 :weight bold :foreground "#008abd"))))
+   `(outline-2 ((,class (:height 1.2 :weight bold :foreground "#7d57c2"))))
+   `(outline-3 ((,class (:height 1.1 :weight bold :foreground "#cc6d00"))))
    `(outline-4 ((,class (:height 1.0 :weight bold :foreground "#2e994c"))))
    `(outline-5 ((,class (:height 1.0 :weight bold :foreground "#00638a"))))
    `(outline-6 ((,class (:height 1.0 :weight bold :foreground "#D0372D"))))
@@ -316,9 +372,9 @@
    `(outline-8 ((,class (:height 1.0 :weight bold :foreground "#6c7378"))))
 
    ;; org
-   `(org-level-1 ((,class (:height 1.0 :weight bold :foreground "#008abd"))))
-   `(org-level-2 ((,class (:height 1.0 :weight bold :foreground "#7d57c2"))))
-   `(org-level-3 ((,class (:height 1.0 :weight bold :foreground "#cc6d00"))))
+   `(org-level-1 ((,class (:height 1.3 :weight bold :foreground "#008abd"))))
+   `(org-level-2 ((,class (:height 1.2 :weight bold :foreground "#7d57c2"))))
+   `(org-level-3 ((,class (:height 1.1 :weight bold :foreground "#cc6d00"))))
    `(org-level-4 ((,class (:height 1.0 :weight bold :foreground "#2e994c"))))
    `(org-level-5 ((,class (:height 1.0 :weight bold :foreground "#00638a"))))
    `(org-level-6 ((,class (:height 1.0 :weight bold :foreground "#D0372D"))))
@@ -342,13 +398,13 @@
    `(highlight-numbers-number ((,class (:foreground "#d75f00"))))
 
    ;; js2
-   `(js2-error ((,class (:box (:line-width 1 :color "#e67373") :background "#ffdddd"))))
-   `(js2-warning ((,class (:underline "#f4ad49"))))
-   `(js2-external-variable ((,class (:foreground "#e67373"))))
+   `(js2-error ((,class (:box (:line-width 1 :color "#D0372D") :background "#ffdddd"))))
+   `(js2-warning ((,class (:underline "#cc6d00"))))
+   `(js2-external-variable ((,class (:foreground "#D0372D"))))
    `(js2-function-param ((,class (:foreground "#008abd"))))
    `(js2-instance-member ((,class (:foreground "#2f7e9d"))))
-   `(js2-jsdoc-html-tag-delimiter ((,class (:foreground "#e67373"))))
-   `(js2-jsdoc-html-tag-name ((,class (:foreground "#e67373"))))
+   `(js2-jsdoc-html-tag-delimiter ((,class (:foreground "#D0372D"))))
+   `(js2-jsdoc-html-tag-name ((,class (:foreground "#D0372D"))))
    `(js2-jsdoc-tag ((,class (:weight normal :foreground "#7d57c2"))))
    `(js2-jsdoc-type ((,class (:foreground "#008abd"))))
    `(js2-jsdoc-value ((,class (:weight normal :foreground "#7d57c2"))))
